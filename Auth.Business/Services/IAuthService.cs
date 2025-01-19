291e1e3524f19a553d966fcd86f0810bfb804cac
rotmayer <r.salnikov1998@gmail.com>
2025-01-19 16:37:14 +1000

    Init commit

---
+++ Auth.Business/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Auth.Business.Models;
+
+namespace Auth.Business.Services;
+
+public interface IAuthService
+{
+    string Register(string login, string password);
+    string Login(string login, string password);
+    string GenerateJwt(JwtHeader header, JwtPayload payload, string secretKey);
+    string ValidateJwt(string jwt);
+}
---
+++ Auth.Domain/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Auth.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Login { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
+    public DateTime? UpdatedAt { get; set; } // nullable потому что при создании модели мы не знаем UpdatedAt
+    public DateTime? CreatedAt { get; set; } // nullable потому что при создании модели мы не знаем CreatedAt
+}
